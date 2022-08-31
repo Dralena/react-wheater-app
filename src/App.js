f3d9cc838571876c462360f2a6f09a0606d0f176
Dralena <103277684+Dralena@users.noreply.github.com>
2022-08-31 09:29:59 +0200

    Added a link to the Github repository

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <h1>Weather App</h1>;
+  return (
+    <div>
+      <h1>Weather App</h1>;
+      <footer>
+        This project was coded by Carmen von Känel and is{" "}
+        <a href="https://github.com/Dralena/react-wheater-app" target="_blank">
+          open-source
+        </a>
+        .{" "}
+      </footer>
+    </div>
+  );
 }
 
 export default App;
