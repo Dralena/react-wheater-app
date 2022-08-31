fe0712b387e69353826702b886cc569fb078cfc7
Dralena <103277684+Dralena@users.noreply.github.com>
2022-08-31 10:01:29 +0200

    Addes bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Weather App</h1>;
+    <div className="container">
+      <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
       <footer>
         This project was coded by Carmen von Känel and is{" "}
-        <a href="https://github.com/Dralena/react-wheater-app" target="_blank">
+        <a
+          href="https://github.com/Dralena/react-wheater-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           open-source
         </a>
         .{" "}
